4efeb139e1ac6188f925e9d6e95dd7f7b4f1730d
AlexandrKryu <a.kryu84@gmail.com>
2022-09-02 22:38:34 +0300

    add fist commit and task54

---
+++ Task54/Program.cs
@@ -0,0 +1,118 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит 
+// по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+int Rows()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество строк: ");
+            int mTmp = int.Parse(Console.ReadLine()!);
+
+            if (mTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return mTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int Colums()
+
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество столбцов: ");
+            int nTmp = int.Parse(Console.ReadLine()!);
+
+            if (nTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return nTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int row = Rows();
+
+int col = Colums();
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+void StringMaxMin(int[,] matrix)
+{
+    int row = matrix.GetLength(0);
+    int col = matrix.GetLength(1);
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            for (int k = 0; k < col - 1; k++)
+            {
+                if (matrix[i, k] < matrix[i, k + 1])
+                {
+                    int temp = matrix[i, k + 1];
+                    matrix[i, k + 1] = matrix[i, k];
+                    matrix[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+int[,] matrix = CreateMtrixRndInt(row, col, 1, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+StringMaxMin(matrix);
+PrintMatrix(matrix);
