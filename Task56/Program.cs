e87d05d6849dc8e5cf176f5d3ba2e3c86f108b19
AlexandrKryu <a.kryu84@gmail.com>
2022-09-03 21:46:10 +0300

    add Task56

---
+++ Task56/Program.cs
@@ -0,0 +1,138 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет 
+// находить строку с наименьшей суммой элементов.
+int Rows()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество строк: ");
+            int mTmp = int.Parse(Console.ReadLine()!);
+
+            if (mTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return mTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int Colums()
+
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество столбцов: ");
+            int nTmp = int.Parse(Console.ReadLine()!);
+
+            if (nTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return nTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int row = Rows();
+
+int col = Colums();
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max);
+        }
+    }
+
+    return matrix;
+}
+
+int[] SumElemRow(int[,] array)
+{
+    int size = Math.Min(array.GetLength(0), array.GetLength(1));
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            result[i] += array[i, j];
+        }
+    }
+    return result;
+}
+
+void MinSumRow(int[] array)
+{
+
+    int temp = array[0];
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (temp > array[i])
+        {
+            temp = array[i];
+            count = i;
+        }
+    }
+    Console.WriteLine($"Строка {count + 1} с наименьшей сумой элементов {temp}");
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]},");
+        else Console.Write(array[i]);
+    }
+    Console.WriteLine("]");
+}
+
+int[,] matrix = CreateMtrixRndInt(row, col, 1, 9);
+PrintMatrix(matrix);
+Console.WriteLine();
+int[] array = SumElemRow(matrix);
+PrintArray(array);
+MinSumRow(array);
+
