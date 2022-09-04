ab6d00fd01916212c38d1c6a75eb1455d5b3b906
AlexandrKryu <a.kryu84@gmail.com>
2022-09-04 23:07:56 +0300

    add Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,104 @@
+﻿// Задача 62. Напишите программу, 
+// которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+
+int Rows()
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество строк: ");
+            int mTmp = int.Parse(Console.ReadLine()!);
+
+            if (mTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return mTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int Colums()
+
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write("Введите количество столбцов: ");
+            int nTmp = int.Parse(Console.ReadLine()!);
+
+            if (nTmp <= 0)
+            {
+                Console.WriteLine("Пожалуйста, введите значение больше нуля!");
+                Console.WriteLine();
+                continue;
+            }
+            else return nTmp;
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка! Некорректный формат! Повторите попытку!");
+            Console.WriteLine();
+            continue;
+        }
+    }
+}
+
+int row = Rows();
+
+int col = Colums();
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) System.Console.Write($"{matrix[i, j],4},");
+            else Console.Write($"{matrix[i, j],4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] SpiralMatrix(int[,] matrix)
+{
+    int temp = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+            {
+                matrix[i, j] = temp;
+                temp++;
+                if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+                    j++;
+                else if (i < j && i + j >= matrix.GetLength(0) - 1)
+                    i++;
+                else if (i >= j && i + j > matrix.GetLength(1) - 1)
+                    j--;
+                else
+                    i--;
+            }
+        }
+    }
+    return matrix;
+}
+
+int[,] matrix = new int[4, 4];
+int[,] matrix1 = SpiralMatrix(matrix);
+Console.WriteLine();
+PrintMatrix(matrix1);
